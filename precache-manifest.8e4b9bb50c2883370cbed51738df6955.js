3cf1271bd1a59353abcfdce6434a5ed2ec278d6a
Anjali B N <bnanjali@gmail.com>
2020-04-16 16:12:41 +0530

    Updates

---
+++ precache-manifest.8e4b9bb50c2883370cbed51738df6955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1613e727255f0ed0afccc1fbefcc69d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "158bf012117006a1176e",
+    "url": "/monsters-rolodex/static/css/main.f5d03bbe.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158bf012117006a1176e",
+    "url": "/monsters-rolodex/static/js/main.958295d6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
